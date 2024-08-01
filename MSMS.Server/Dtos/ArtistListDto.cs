e225a69e0dc1aa675b606fe90d734873df83f552
ShantanuBhatia <sb5455@nyu.edu>
2024-08-01 17:15:43 +0400

    comment cleanup in DTOs

--- MSMS.Server/Dtos/ArtistListDto.cs
+++ MSMS.Server/Dtos/ArtistListDto.cs
@@ -7,7 +7,6 @@ namespace MSMS.Server.Dtos
         public int ArtistListId { get; set; }
         public string ArtistListName { get; set; } = string.Empty;
         public string UserId { get; set; } = string.Empty;
-        // TODO figure out how this model should actually reference artists
         public List<ArtistDto> Artists { get; set; } = new List<ArtistDto>();
     }
 }
