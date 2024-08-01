c4fe0e9619f1502203952fe98e29cf247a2dc6bc
ShantanuBhatia <sb5455@nyu.edu>
2024-08-01 17:15:32 +0400

    deleted boilerplate files and added dummy endpoint

--- MSMS.Server/ArtistList.cs
+++ MSMS.Server/ArtistList.cs
@@ -3,6 +3,7 @@
     public class ArtistList
     {
         public int ArtistListId { get; set; }
+        public string ArtistListName { get; set; }
         public int UserId { get; set; }
         public List<Artist> Artists { get; set; } = [];
     }
---
+++ MSMS.Server/Controllers/ArtistListController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using MSMS.Server.Models;
+
+namespace MSMS.Server.Controllers
+{
+    [ApiController]
+    [Route("api/artistlists")]
+    public class ArtistListController : ControllerBase
+    {
+        private static readonly string[] TempArtists = ["Stray Kids", "RESCENE", "NewJeans", "LeSserafim"];
+
+        private readonly ILogger<ArtistListController> _logger;
+
+        public ArtistListController(ILogger<ArtistListController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetArtistListById([FromRoute] int id)
+        {
+            if (id == 999)
+            {
+                return NotFound();
+            }
+
+            List<Artist> artists = new List<Artist>();
+            for (int i = 0; i < TempArtists.Length; i++) 
+            { 
+                artists.Add(new Artist { ArtistDisplayName = TempArtists[i], ArtistId=i+200});
+            }
+            ArtistList returnObject = new ArtistList
+            {
+                ArtistListName = "Example Artist List Number " + id.ToString(),
+                Artists = artists,
+                ArtistListId = Random.Shared.Next(-20, 55),
+                UserId = 42069
+            };
+            return Ok(returnObject);
+
+        }
+    }
+}
