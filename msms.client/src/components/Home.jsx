889e847df53979210deacecb73eaa2b5e88b9789
ShantanuBhatia <sb5455@nyu.edu>
2024-08-15 16:36:42 +0400

    added handling for unauthed user

--- msms.client/src/components/Home.jsx
+++ msms.client/src/components/Home.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { artistListApi } from '../services/apiService';
 import ArtistListList from './ArtistListList';
 import heroBanner from "../assets/herobanner.jpg";
@@ -12,6 +12,16 @@ const Home = ({ user, isMobile }) => {
     const [artistLists, setArtistLists] = useState([]);
     const [placeholderIndex, setPlaceholderIndex] = useState(0);
     const [lineupName, setLineupName] = useState("");
+    const [showInterestModal, setShowInterestModal] = useState(null);
+    const [userEmail, setUserEmail] = useState("");
+
+    const location = useLocation();
+
+    useEffect(() => {
+        if (location.state?.showInterestModal) {
+            setShowInterestModal(true);
+        }
+    }, [location]);
     
 
     useEffect(() => {
@@ -44,10 +54,47 @@ const Home = ({ user, isMobile }) => {
         return (() => { clearInterval(timer); })
     }, []);
 
+    const InterestModal = (
+        <div className="fixed inset-0 z-50 overflow-auto bg-black bg-opacity-50 flex items-center justify-center">
+            <div className="relative bg-[#111813] w-full max-w-md m-auto flex-col flex rounded-lg shadow-lg">
+                <div className="px-6 py-4">
+                    <h2 className="text-white text-xl font-bold mb-4">You just wait, sunshine...</h2>
+                    <p className="text-white text-base mb-4">Lineup is currently in limited release - we&apos;ll let you know when it&apos;s ready for you</p>
+                    <input
+                        type="email"
+                        placeholder="user@example.com"
+                        maxLength="32"
+                        onChange={(e) => { setUserEmail(e.target.value) }}
+                        value={userEmail}
+                        className="w-full p-2 mb-4 border rounded text-gray-200 bg-gray-700 border-gray-600"
+                    />
+                    <div className="flex justify-end">
+                        <button
+                            className="bg-[#19cc58] text-[#111813] rounded-full font-bold hover:bg-[#16b850] px-4 py-2"
+                            onClick={() => setShowInterestModal(false)}
+                        >
+                            Submit
+                        </button>
+                    </div>
+                </div>
+                <button
+                    className="absolute top-0 right-0 mt-4 mr-4 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
+                    onClick={() => setShowInterestModal(false)}
+                >
+                    <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
+            </div>
+        </div>
+    );
 
     return (
         <div className={`flex flex-col flex-1 ${isMobile ? 'px-4' : 'px-24'} py-5`}>
-            <FadeInSection>
+
+            {showInterestModal && <>{InterestModal}</>}
+            
+            <FadeInSection delay={0.2}>
                 <div className="max-w-[960px] mx-auto w-full">
                     <div className={isMobile ? 'p-2' : 'p-4'}>
                         <div
